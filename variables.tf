7fdc7a5c3d9cd4b3d5c9fde0b1dd225ac94677a4
Saurabh3255 <118536749+Saurabh3255@users.noreply.github.com>
2024-03-03 22:58:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Desktop.pem" 
+    default     =  "Desktop" 
 }
 
 variable "instance_type" { 
