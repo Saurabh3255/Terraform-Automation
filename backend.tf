f73febf93c7a21201ce14236c5ff6d0dbfdea908
Saurabh3255 <118536749+Saurabh3255@users.noreply.github.com>
2024-03-03 22:44:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-tf-state-bucket-project-terraform-infra-batch-22"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamo-db-project-infra"
   }
 }
